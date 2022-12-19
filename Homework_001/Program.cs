b7ea7ec08212eb59154452c458e48090caea4f6a
Evgenii Popov <saip0904@gmail.com>
2022-12-19 15:08:47 +0500

    Нахождение наибольшего числа из двух переменных

--- Homework_001/Program.cs
+++ Homework_001/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите два числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"Наибольшее число {a}");
+}
+else
+{
+    Console.WriteLine($"Наибольшее число {b}");
+}
+
+
