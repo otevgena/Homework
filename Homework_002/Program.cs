f1b6f808930c327f26b7c4afa8c188084180794f
Evgenii Popov <saip0904@gmail.com>
2022-12-19 15:41:22 +0500

    Нахождение наибольшего числа из трех  переменных

---
+++ Homework_002/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите три числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if ((a > b) && (a > c)) 
+{
+    Console.WriteLine($"Наибольшее число {a}");
+}
+if ((b > a) && (b > c)) 
+{
+    Console.WriteLine($"Наибольшее число {b}");
+}
+if ((c > a) && (c > b))
+{
+    Console.WriteLine($"Наибольшее число {c}");
+}
