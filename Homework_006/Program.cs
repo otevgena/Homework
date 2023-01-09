6951a52f624c782601072f87664afc840545c275
Evgenii Popov <saip0904@gmail.com>
2023-01-09 13:08:30 +0500

    Программа для определения третьей цифры числа с проверкой на длину числа

---
+++ Homework_006/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("Введите трехзначное число");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+if (stringNumber.Length > 2) {
+    Console.WriteLine("Третяя цифра этого чиcла "+ stringNumber[2]);
+}
+else {
+    Console.WriteLine("Нет третьей цифры");
+}
